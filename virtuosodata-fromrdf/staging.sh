b6f01957be7c65d55dcd32d3c3b9cf698ddbd27c
QIU Quan <jackqq@gmail.com>
2017-02-26 14:00:00 +0800

    Make Virtuoso data importing start to work.

---
+++ virtuosodata-fromrdf/staging.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Exit on first error
+set -e
+
+function isql {
+ /usr/local/bin/isql ops-virtuoso dba dba VERBOSE=OFF BANNER=OFF PROMPT=OFF ECHO=OFF BLOBS=ON ERRORS=stdout "$@"
+}
+
+
+# FIXME: Is this needed? It seems overly permissive..
+  echo "Configuring SPARQL"
+  isql 'exec=GRANT EXECUTE ON DB.DBA.SPARQL_INSERT_DICT_CONTENT TO "SPARQL";'
+  isql 'exec=GRANT EXECUTE ON DB.DBA.L_O_LOOK TO "SPARQL";'
+  isql 'exec=GRANT EXECUTE ON DB.DBA.SPARUL_RUN TO "SPARQL";'
+  isql 'exec=GRANT EXECUTE ON DB.DBA.SPARQL_DELETE_DICT_CONTENT TO "SPARQL";'
+  isql 'exec=GRANT EXECUTE ON DB.DBA.RDF_OBJ_ADD_KEYWORD_FOR_GRAPH TO "SPARQL";'
+
+echo "Populating from /staging/staging.sql"
+isql /staging/staging.sql
+
